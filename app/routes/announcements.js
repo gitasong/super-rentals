9f78f37a2307007eff779ca9d61759cdbe1c312d
Michelle Poterek and Nicole Freed <michelle@>
2017-08-07 13:55:56 -0700

    add type property to announcement model

--- app/routes/announcements.js
+++ app/routes/announcements.js
@@ -2,14 +2,17 @@ import Ember from 'ember';
 
 var announcements = [{
   id: 1,
+  type: "warning",
   location: "Acapulco, Mexico",
   message: "Tropical storm warning; residents told to evacuate"
 }, {
   id: 2,
+  type: "info",
   location: "Pago Pago, American Samoa",
   message: "Smoke signals seen in distance"
 }, {
   id: 3,
+  type: "warning",
   location: "Canberra, Australia",
   message: "Town overrun by koala bears"
 }];
