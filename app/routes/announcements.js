a9f9e382700d5592c02baf0f9809c265833e89ec
Michelle Poterek and Nicole Freed <nicole@>
2017-08-07 14:41:59 -0700

    return all from database

--- app/routes/announcements.js
+++ app/routes/announcements.js
@@ -1,24 +1,7 @@
 import Ember from 'ember';
 
-var announcements = [{
-  id: 1,
-  type: "warning",
-  location: "Acapulco, Mexico",
-  message: "Tropical storm warning; residents told to evacuate"
-}, {
-  id: 2,
-  type: "info",
-  location: "Pago Pago, American Samoa",
-  message: "Smoke signals seen in distance"
-}, {
-  id: 3,
-  type: "warning",
-  location: "Canberra, Australia",
-  message: "Town overrun by koala bears"
-}];
-
 export default Ember.Route.extend({
   model() {
-    return announcements;
+    return this.store.findAll('announcement');
   },
 });
