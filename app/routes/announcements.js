f72fab06467351af1237d75a0bc15f68099d9b94
Michelle Poterek and Nicole Freed <nicole@>
2017-08-07 16:29:59 -0700

    create destroy announcement button + action

--- app/routes/announcements.js
+++ app/routes/announcements.js
@@ -4,4 +4,12 @@ export default Ember.Route.extend({
   model() {
     return this.store.findAll('announcement');
   },
+
+  actions: {
+    destroyAnnouncement(announcement) {
+      console.log('hello');
+      announcement.destroyRecord();
+      this.transitionTo('announcements');
+    }
+  }
 });
