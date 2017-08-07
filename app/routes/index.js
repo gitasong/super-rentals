314c9b50fc1aa8ba038feb95a4a71b5f4e3c8655
Michelle Poterek and Nicole Freed <nicole@>
2017-08-07 13:51:03 -0700

    delete announcments model from index.js and announcements display loop from index.hbs

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,22 +1,7 @@
 import Ember from 'ember';
 
-var announcements = [{
-  id: 1,
-  location: "Acapulco, Mexico",
-  message: "Tropical storm warning; residents told to evacuate"
-}, {
-  id: 2,
-  location: "Pago Pago, American Samoa",
-  message: "Smoke signals seen in distance"
-}, {
-  id: 3,
-  location: "Canberra, Australia",
-  message: "Town overrun by koala bears"
-}];
-
 export default Ember.Route.extend({
   model() {
-    return announcements;
     return this.store.findAll('rental');
   },
 
