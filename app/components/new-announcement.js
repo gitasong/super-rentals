87c3b90b9065376659ce2659b831e11a6653be7a
Max Scher and Nicole Freed <max@>
2017-08-08 08:36:24 -0700

    add saveAnnouncement() action

--- app/components/new-announcement.js
+++ app/components/new-announcement.js
@@ -5,6 +5,16 @@ export default Ember.Component.extend({
   actions: {
     announcementFormShow() {
       this.set('addNewAnnouncement', true);
+    },
+
+    saveAnnouncement() {
+      var params = {
+        type: this.get('type'),
+        location: this.get('location'),
+        message: this.get('message'),
+      };
+      this.set('addNewAnnouncement', false);
+      this.sendAction('saveAnnouncement', params);
     }
   }
 });
