2d081a05d3bf6752bdad7725cbe7f1661e0bf1ad
Max Scher and Nicole Freed <nicole@>
2017-08-08 08:26:09 -0700

    create action to show announcement form

--- app/components/new-announcement.js
+++ app/components/new-announcement.js
@@ -1,4 +1,10 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  addNewAnnouncment: false,
+  actions: {
+    announcementFormShow() {
+      this.set('addNewAnnouncment', true);
+    }
+  }
 });
