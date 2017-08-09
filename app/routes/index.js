37319ccefd0d2748e6ad85e882741da0a37cf92b
Nicole Freed <nicole.freed@gmail.com>
2017-08-09 16:03:29 -0700

    fix typos in index.js

--- app/routes/index.js
+++ app/routes/index.js
@@ -3,9 +3,9 @@ import Ember from 'ember';
 export default Ember.Route.extend({
   model() {
     return Ember.RSVP.hash({
-      rentals: this.store.findAll('rental');
-      reviews: this.store.findAll('review');
-    })
+      rentals: this.store.findAll('rental'),
+      reviews: this.store.findAll('review')
+    });
   },
 
   actions: {
